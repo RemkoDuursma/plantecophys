397e7d597b998af18a704f4bb4e28b97ea461c3b
Remko Duursma <remkoduursma@gmail.com>
2013-06-03 10:14:49 +1000

    Packaged. DewtoVPD moved.

--- R/RHtoVPD.R
+++ R/RHtoVPD.R
@@ -1,12 +1,13 @@
-#' Conversions between relative humidity and vapour pressure deficit
+#' Conversions between relative humidity, vapour pressure deficit and dewpoint
 #' 
-#' @description Converts relative humidity (%) to vapour pressure deficit (kPa), or vice versa. To convert from relative humidity, use the \code{RHtoVPD} function, use \code{VPDtoRH} for the other way around. The water vapor saturation pressure is calculated with \code{esat}, which is from Jones 1992.
-#' @param RH Relative humidity (%)
+#' @description Converts relative humidity (\%) to vapour pressure deficit (kPa), or vice versa. To convert from relative humidity, use the \code{RHtoVPD} function, use \code{VPDtoRH} for the other way around. The water vapor saturation pressure is calculated with \code{esat}, which is from Jones 1992. Also provided is \code{DewtoVPD} to convert from dewpoint temperature to VPD.
+#' @param RH Relative humidity (\%)
 #' @param TdegC Temperature (degrees C)
 #' @param VPD Vapour pressure deficit (kPa)
 #' @param Pa Atmospheric pressure
-#' @export
-#' @aliases RHtoVPD VPDtoRH esat
+#' @param Tdew Dewpoint temperature
+#' @export RHtoVPD VPDtoRH esat DewtoVPD
+#' @aliases RHtoVPD VPDtoRH esat DewtoVPD
 #' @references Jones, H.G. 1992. Plants and microclimate: a quantitative approach to environmental plant physiology. 2nd Edition., 2nd Edn. Cambridge University Press, Cambridge. 428 p.
 #' @author Remko Duursma
 RHtoVPD <- function(RH, TdegC, Pa=101){
@@ -15,14 +16,12 @@ RHtoVPD <- function(RH, TdegC, Pa=101){
 	VPD <- (esatval - e)/1000
 return(VPD)
 }
-
 VPDtoRH <- function(VPD, TdegC, Pa=101){
   esatval <- esat(TdegC)
   e <- pmax(0, esatval - VPD*1000)
   RH <- 100 * e/esatval
   return(RH)
 }
-
 esat <- function(TdegC, Pa=101){  
   a <- 611.21
   b <- 17.502
@@ -30,4 +29,14 @@ esat <- function(TdegC, Pa=101){
   f <- 1.0007 + 3.46 * 10^-8 * Pa * 1000
   esatval <- f * a * (exp(b * TdegC/(c + TdegC)))
   return(esatval)
+}
+DewtoVPD <- function(Tdew, TdegC, Pa=101){
+  
+  # Actual vapor pressure.
+  e <- esat(Tdew)
+  
+  # saturated:
+  esatval <- esat(TdegC)
+  
+  return((esatval - e)/1000)
 }
