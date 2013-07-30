cf701feaa2a0d0aa2bbd693e7f995172af4664ba
Remko Duursma <remkoduursma@gmail.com>
2013-07-31 09:47:42 +1000

    Added 'adderrorbars'

---
+++ R/adderrorbars.R
@@ -0,0 +1,35 @@
+#' Adds error bars to a plot
+#' 
+#' @description Yet another function that adds error bars. The user must specify the length of the error bars.
+#' @param x The x coordinates of start of the error bar
+#' @param y The y coordinates of start of the error bar
+#' @param SE The length of the error bar
+#' @param direction One of 'up', 'down', 'right', 'left', 'updown' or 'rightleft'.
+#' @param barlen The length of the cross-bar at the end of the error bar.
+#' @param \ldots Additional parameters passed to \code{\link{arrows}}, such as the colour (\code{col}).
+#' #' @details Simple wrapper for \code{\link{arrows}}, where \code{angle=90} and \code{code=3}. The \code{barlen} argument corresponds to \code{length} in \code{arrows}.
+#' @examples
+#' # A simple example. Also note that we can specify the colour of the error bars, or other parameters
+#' # that arrows() recognizes.
+#' x <- rnorm(20)
+#' y <- x + rnorm(20)
+#' se <- runif(20, 0.2,0.4)
+#' plot(x,y,pch=21,bg="white",panel.first=adderrorbars(x,y,se,direction="updown", col="darkgrey"))
+#' @export
+adderrorbars <- function(x,y,SE,direction,barlen=0.04,...){
+  
+  if(length(direction)>1)stop("direction must be of length one.")
+  if(direction == "updown")
+    direction <- c("up","down")
+  else if(direction == "rightleft")direction <- c("left","right")
+  
+  if("up" %in% direction)
+    arrows(x0=x, x1=x, y0=y, y1=y+SE, code=3, angle=90, length=barlen,...)
+  if("down" %in% direction) 
+    arrows(x0=x, x1=x, y0=y, y1=y-SE, code=3, angle=90, length=barlen,...)
+  if("left" %in% direction) 
+    arrows(x0=x, x1=x-SE, y0=y, y1=y, code=3, angle=90, length=barlen,...)
+  if("right" %in% direction)
+    arrows(x0=x, x1=x+SE, y0=y, y1=y, code=3, angle=90, length=barlen,...)  
+  
+}
