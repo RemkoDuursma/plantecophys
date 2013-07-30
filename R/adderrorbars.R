34db4a075541f52499ad50e270f3be48359a5d39
Remko Duursma <remkoduursma@gmail.com>
2013-07-31 09:52:06 +1000

    rightleft, leftright both work in adderrorbars

--- R/adderrorbars.R
+++ R/adderrorbars.R
@@ -21,7 +21,7 @@ adderrorbars <- function(x,y,SE,direction,barlen=0.04,...){
   if(length(direction)>1)stop("direction must be of length one.")
   if(direction == "updown")
     direction <- c("up","down")
-  else if(direction == "rightleft")direction <- c("left","right")
+  else if(direction == "rightleft" | direction == "leftright")direction <- c("left","right")
   
   if("up" %in% direction)
     arrows(x0=x, x1=x, y0=y, y1=y+SE, code=3, angle=90, length=barlen,...)
