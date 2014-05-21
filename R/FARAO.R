1a82dce8028d9003ba119c34334607e2110b62e8
Remko Duursma <remkoduursma@gmail.com>
2014-05-21 14:32:10 +1000

    Added OPTfunEB for issue #5 , but FARAO does not use it yet. FARAO still works as before here...

--- R/FARAO.R
+++ R/FARAO.R
@@ -3,7 +3,6 @@
 #' @description The numerical solution of the optimal stomatal conductance model, coupled with the Farquhar model of photosynthesis. The model of Medlyn et al. (2011) is an approximation to this full numeric solution.
 #' @param lambda The marginal cost of water (mol mol-1)
 #' @param Ca The CO2 concentration. 
-#' @param gbl Optional, the boundary layer conductance (mol m-2 s-1). Somewhat experimental!
 #' @param photo Which photosynthesis rate should stomata respond to? Defaults to 'BOTH', i.e. the minimum of Vcmax and Jmax limited rates.
 #' @param C4 If TRUE, uses the C4 photosynthesis routine (\code{\link{AciC4}})
 #' @param ... All other parameters are passed to \code{\link{Aci}}
@@ -14,7 +13,7 @@
 #' 
 #' Medlyn, B.E., R.A. Duursma, D. Eamus, D.S. Ellsworth, I.C. Prentice, C.V.M. Barton, K.Y. Crous, P. De Angelis, M. Freeman and L. Wingate. 2011. Reconciling the optimal and empirical approaches to modelling stomatal conductance. Global Change Biology. 17:2134-2144.
 #' @export
-FARAO <- function(lambda=0.002, Ca=400, VPD=1, gbl=NA, 
+FARAO <- function(lambda=0.002, Ca=400, VPD=1, 
                   photo=c("BOTH","VCMAX","JMAX"), C4=FALSE, ...){
   
   photo <- match.arg(photo)
@@ -22,10 +21,10 @@ FARAO <- function(lambda=0.002, Ca=400, VPD=1, gbl=NA,
   fx <- function(Ca,...)optimize(OPTfun, interval=c(0,Ca), 
                                  maximum=TRUE,Ca=Ca,
                                  ...)$maximum
-  optimalcis <- mapply(fx,Ca=Ca,lambda=lambda, gbl=gbl, photo=photo, C4=C4, VPD=VPD,...)
+  optimalcis <- mapply(fx,Ca=Ca,lambda=lambda, photo=photo, C4=C4, VPD=VPD,...)
   
   res <- as.data.frame(OPTfun(Ci=optimalcis, retobjfun=FALSE, 
-                              Ca=Ca,gbl=gbl, photo=photo, C4=C4, VPD=VPD,...))
+                              Ca=Ca, photo=photo, C4=C4, VPD=VPD,...))
   
   return(res)
 }
@@ -38,12 +37,13 @@ OPTfun <- function(Ci,              # mu mol mol-1
                     VPD=1.5,        # kPa
 					          Patm=101,         # ambient pressure, kPa
 					          photo=c("BOTH","VCMAX","JMAX"),
+                    energybalance=FALSE,
                     retobjfun=TRUE, # if false, returns A, g and E (otherwise sum(A-lambda*E))
 					          C4=FALSE,
 					          ...){     
 
   GCtoGW <- 1.57
-	VPDmol <- VPD/Pa
+	VPDmol <- VPD/Patm
 	
 	photo <- match.arg(photo)
 	
@@ -59,10 +59,11 @@ OPTfun <- function(Ci,              # mu mol mol-1
   
   # Given Ci and A, calculate gs (diffusion constraint)
   gs <- GCtoGW * A / (Ca - Ci)
-	
+	    
   # Transpiration rate
-  E <- gtot*VPDmol
-  
+  E <- gs*VPDmol
+    
+    
   # Objective function to be maximized (Cowan-Farquhar condition)
   objfun <- 10^-6*A - lambda*E
 
@@ -71,4 +72,82 @@ if(retobjfun)return(objfun)
 if(!retobjfun)return(list( Ci=Ci, ALEAF=A, GS=gs, ELEAF=E*1000, Ac=run$Ac, Aj=run$Aj,
                            Rd=run$Rd, VPD=VPD, Tleaf=run$Tleaf,  Ca=Ca, PPFD=run$PPFD ))
 }         
+
+
+OPTfunEB <- function(Ci,           # mu mol mol-1
+                   lambda=0.002,   # mol mol-1
+                   Ca=400,         # mu mol mol-1
+                   VPD=1.5,        # AIR VPD! kPa
+                   Patm=101,       # ambient pressure, kPa
+                   Tair=25,
+                   Wind=2,
+                   Wleaf=0.02,
+                   StomatalRatio=1,
+                   LeafAbs=0.86,
+                   photo=c("BOTH","VCMAX","JMAX"),
+                   energybalance=FALSE,
+                   retobjfun=TRUE, # if false, returns A, g and E (otherwise sum(A-lambda*E))
+                   C4=FALSE,
+                   ...){     
+  
+  GCtoGW <- 1.57
+  VPDmol <- VPD/Patm
+  
+  photo <- match.arg(photo)
+  
+  gsfun <- function(Ci, VPD, returnwhat=c("gs","all"), ...){
+    
+    returnwhat <- match.arg(returnwhat)
+    
+    # Given a Ci, calculate photosynthetic rate
+    if(!C4)
+      run <- Aci(Ci, VPD=VPD, ...)   # note that VPD does not do anything, just for consistency in I/O
+    else 
+      run <- AciC4(Ci, VPD=VPD, ...)
+    
+    if(photo == "BOTH")A <- run$ALEAF
+    if(photo == "VCMAX")A <- run$Ac
+    if(photo == "JMAX")A <- run$Aj
+    
+    # Given Ci and A, calculate gs (diffusion constraint)
+    gs <- GCtoGW * A / (Ca - Ci)
+    
+  if(returnwhat == "gs")return(gs)
+  if(returnwhat == "all")return(list(run=run, GS=gs, A=A))
+  }
+  
+  # Find Tleaf. Here, we take into account that Tleaf as solved from
+  # energy balance affects gs (because it affects)
+  fx <- function(x, Ci, Tair, Wind, VPD, Wleaf, StomatalRatio, LeafAbs, ...){
+    newx <- FindTleaf(Tair=Tair, gs=gsfun(Ci=Ci, Tleaf=x, VPD=VPD, ...), 
+                      Wind=Wind, Wleaf=Wleaf, 
+                      StomatalRatio=StomatalRatio, LeafAbs=LeafAbs)
+    newx - x
+  }
+  Tleaf <- uniroot(fx, interval=c(Tair-15, Tair+15), Ci=Ci, Tair=Tair, Wind=Wind, VPD=VPD, Wleaf=Wleaf, 
+                   StomatalRatio=StomatalRatio, LeafAbs=LeafAbs, ...)$root
+  z <- gsfun(Ci=Ci, Tleaf=Tleaf, VPD=VPD, returnwhat="all",...)
+  GS <- z$GS
+  A <- z$A
+  
+  # And energy balance components
+  e <- LeafEnergyBalance(Tleaf=Tleaf, Tair=Tair, gs=GS, 
+                         PPFD=z$run$PPFD, VPD=VPD, Patm=z$run$Patm, 
+                         Wind=Wind, Wleaf=Wleaf, 
+                         StomatalRatio=StomatalRatio, LeafAbs=LeafAbs,
+                         returnwhat="fluxes")
+
+  E <- e$ELEAFeb
+  
+  # Objective function to be maximized (Cowan-Farquhar condition)
+  objfun <- 10^-6*A - lambda*E/1000
+  
+  if(retobjfun)return(objfun)
+  
+  if(!retobjfun)return(list( Ci=Ci, ALEAF=A, GS=gs, ELEAF=E*1000, Ac=z$run$Ac, Aj=z$run$Aj,
+                             Rd=z$run$Rd, VPD=VPD, Tleaf=Tleaf,  Ca=Ca, PPFD=z$run$PPFD ))
+}  
+
+
+
 
--- photosyn_test/photosynebtest.R
+++ photosyn_test/photosynebtest.R
@@ -21,3 +21,14 @@ with(x, plot(VPDleaf, ALEAF/ELEAF, pch=19, col=as.factor(Wind),
 legend("topright",  levels(as.factor(x$Wind)), title="Wind speed", col=palette(),pch=19)
 with(y[order(y$VPD),], points(VPD, ALEAF/ELEAF, type='l'))
 legend("top", c("Photosyn","PhotosynEB"), lty=c(1,-1), pch=c(-1,19))
+
+
+
+
+vcmaxs <- seq(10, 200, length=25)
+jmaxs <- 2 * vcmaxs
+
+run <- PhotosynEB(Vcmax=vcmaxs, Jmax=jmaxs, VPD=2, Tair=25, PPFD=1800, Wind=0.1, Wleaf=0.1)
+
+with(run, plot(GS, ALEAF, type='o'))
+abline(lm(ALEAF ~ GS, data=run[1:3,]), col="red")
