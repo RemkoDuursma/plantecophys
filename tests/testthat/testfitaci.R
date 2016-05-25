6f3e0440555e08a05793579f31eebd3df5a864f6
Remko Duursma <remkoduursma@gmail.com>
2016-05-25 11:47:18 +1000

    Added test unit

---
+++ tests/testthat/testfitaci.R
@@ -0,0 +1,44 @@
+
+library(plantecophys)
+context("Fit ACi curves")
+
+# Load example dataset
+acidat <- read.csv("ACi_court2.csv")
+acidat <- droplevels(acidat[acidat$ID %in% levels(acidat$ID)[1:10],])
+vn <- list(ALEAF="Ps",Tleaf="Temp",Ci="Ci",PPFD="PPFD")
+
+# Fit one A-Ci curve
+acidatone <- subset(acidat, ID == "1000_2_3")
+fit1 <- fitaci(acidatone, varnames=vn, fitmethod="default")
+fit2 <- fitaci(acidatone, varnames=vn, fitmethod="gulike")
+fit3 <- fitaci(acidatone, varnames=vn, fitmethod="gulike", Tcorrect=TRUE, Tleaf=15)
+fit4 <- fitaci(acidatone, varnames=vn, fitmethod="gulike", Tcorrect=FALSE, Tleaf=15)
+
+# With mesophyll conductance
+fit5 <- fitaci(acidatone, varnames=vn, fitmethod="default", gmeso=0.9)
+fit6 <- fitaci(acidatone, varnames=vn, fitmethod="default", gmeso=0.3)
+
+# Fit many A-Ci curves
+fits1 <- fitacis(acidat, "ID", varnames=vn, fitmethod="default", progressbar=FALSE)
+fits2 <- fitacis(acidat, "ID", varnames=vn, fitmethod="gulike", progressbar=FALSE)
+
+
+test_that("Aci curve fit output format", {
+  expect_equal(names(coef(fit1)), c("Vcmax","Jmax","Rd"))
+  expect_equal(names(coef(fit2)), c("Vcmax","Jmax","Rd"))
+  expect_equal(names(coef(fits1)), c("ID","Vcmax","Jmax","Rd","Vcmax_SE","Jmax_SE","Rd_SE"))
+  expect_equal(names(coef(fits2)), c("ID","Vcmax","Jmax","Rd","Vcmax_SE","Jmax_SE","Rd_SE"))
+  expect_equal(nrow(coef(fits1)), nlevels(acidat$ID))
+  expect_equal(nrow(coef(fits2)), nlevels(acidat$ID))
+  expect_s3_class(fit1, "acifit")
+  expect_s3_class(fits1, "acifits")
+})
+
+test_that("Aci curve fitted coefficients",{
+  expect_lt(coef(fit1)[1], coef(fit1)[2], "Vcmax","Jmax")
+  expect_lt(coef(fit2)[1], coef(fit2)[2], "Vcmax","Jmax")
+  expect_lt(coef(fit3)[2], coef(fit4)[2], "Jmax_Tcorrect", "Jmax_noTcorrect")
+  expect_gt(min(coef(fit1)), 0)
+  expect_gt(min(coef(fit2)), 0)
+  expect_gt(coef(fit6)[1], coef(fit5)[1], "Vcmax gmeso = 0.3", "Vcmax gmeso = 0.3")
+})
